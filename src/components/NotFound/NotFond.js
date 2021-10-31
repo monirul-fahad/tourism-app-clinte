20f738a35a1bd6ceb9fa341458daf356038ba1f2
Monirul Fahad <immonirulfahad@gmail.com>
2021-10-31 23:58:57 +0600

    push

--- src/components/NotFound/NotFond.js
+++ src/components/NotFound/NotFond.js
@@ -1,10 +1,17 @@
 import React from "react";
-
+import "./NotFound.css";
 const NotFond = () => {
   return (
-    <div>
-      <h1>404</h1>
-      <h5>Not Found</h5>
+    <div className="error">
+      <div className="container text-center mt-5 pt-5 text-danger">
+        <h1 className="h1">404</h1>
+        <p className="p">Not Found</p>
+        <p className="p2">
+          <small>
+            The resource requested could not be found on this server.
+          </small>
+        </p>
+      </div>
     </div>
   );
 };
